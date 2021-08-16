648b33d1b914b763cdfdd846619d6f36b23b61cf
yutaro_elk <36399275+yutaro-elk@users.noreply.github.com>
2021-08-16 15:06:29 +0900

    refactor: CSSのリファクタ, typo

--- src/components/ReportRefereeList/index.vue
+++ src/components/ReportRefereeList/index.vue
@@ -1,50 +1,46 @@
 <template>
   <section class="referee">
-    <el-row class="content header">
-      <el-col :span="4" />
-      <el-col :span="20" style="display: flex;align-items: center">
-        <div class="referee-column" style="width: 130px">
-          [時期]関係性
-        </div>
-        <div class="referee-column" style="width: 200px">
-          一緒に仕事をした<span class="non-break">期間</span>
-        </div>
-        <div class="referee-column" style="width: 240px">
-          一緒に仕事をしたときの<span class="non-break">推薦者の会社名</span>
-        </div>
-        <div class="referee-column" style="width: 240px">
-          一緒に仕事をしたときの<span class="non-break">推薦者の職種</span>
-        </div>
-        <div class="referee-column" style="width: 200px">
-          所属
-        </div>
-      </el-col>
-    </el-row>
-    <el-row v-for="(referee, i) in referees" :key="i" :gutter="20" class="referee-row">
-      <el-col :span="4">
+    <div class="header">
+      <div class="column name-status" />
+      <div class="column relationship">
+        [時期]関係性
+      </div>
+      <div class="column duration">
+        一緒に仕事をした<span class="non-break">期間</span>
+      </div>
+      <div class="column company">
+        一緒に仕事をしたときの<span class="non-break">推薦者の会社名</span>
+      </div>
+      <div class="column jobCategory">
+        一緒に仕事をしたときの<span class="non-break">推薦者の職種</span>
+      </div>
+      <div class="column belongs">
+        所属
+      </div>
+    </div>
+    <div v-for="(referee, i) in referees" :key="i" class="row">
+      <div class="column name-status">
         <span class="name">{{ referee.name }} </span>
-        <el-tag v-if="referee.completedAt" size="small" type="success" effect="dark" style="font-weight: bold">完了</el-tag>
-        <el-tag v-else-if="referee.answer" size="small" effect="dark" style="font-weight: bold">途中</el-tag>
-        <el-tag v-else size="small" type="info">未回答</el-tag>
-      </el-col>
-      <el-col :span="20" style="display: flex">
-        <div class="referee-column" style="width: 130px">
-          <p>[{{ getTimeWorkingTogetherLabel(referee) }}]<span class="non-break">{{ getRelationshipLabel(referee) }}</span></p>
-        </div>
-        <div class="referee-column" style="width: 200px">
-          <p>{{ getDuration(referee) }}</p>
-        </div>
-        <div class="referee-column" style=" width: 240px">
-          <p>{{ referee.company }}</p>
-        </div>
-        <div class="referee-column" style=" width: 240px">
-          <p>{{ getJobCategoryLavel(referee.jobCategory) }}</p>
-        </div>
-        <div class="referee-column" style="width: 200px">
-          <p>{{ referee.belongs }}</p>
-        </div>
-      </el-col>
-    </el-row>
+        <el-tag v-if="referee.completedAt" type="success" effect="dark">完了</el-tag>
+        <el-tag v-else-if="referee.answer" effect="dark">途中</el-tag>
+        <el-tag v-else type="info">未回答</el-tag>
+      </div>
+      <div class="column relationship">
+        [{{ getTimeWorkingTogetherLabel(referee) }}]<span class="non-break">{{ getRelationshipLabel(referee) }}</span>
+      </div>
+      <div class="column duration">
+        {{ getDuration(referee) }}
+      </div>
+      <div class="column company">
+        {{ referee.company }}
+      </div>
+      <div class="column jobCategory">
+        {{ getJobCategoryLabel(referee.jobCategory) }}
+      </div>
+      <div class="column belongs">
+        {{ referee.belongs }}
+      </div>
+    </div>
   </section>
 </template>
 
@@ -104,7 +100,7 @@ export default {
       const endStr = endDate ? dayjs(endDate).format('YYYY/MM/DD') : ''
       return `${beginStr} ~ ${endStr}`
     },
-    getJobCategoryLavel(jobCategory) {
+    getJobCategoryLabel(jobCategory) {
       const getFilteredArray = (target, values) => {
         return arrayTreeFilter(
           target,
@@ -139,29 +135,31 @@ export default {
   align-items: center
   font-size: 0.85rem
   font-weight: bold
-  .referee-column
-    color: #909399
-.referee
-  padding-bottom: -10px
-  &-row
-    padding-top: 16px
-    padding-bottom: 8px
-    display: flex
-    &+&
-      border-top: 1px solid #EBEEF5
-  &-column
-    &+&
-      margin-left: 16px
-    p
-      margin-top: 0
-      margin-bottom: 0
-      color: #606266
-      font-size: 14px
-.name
-  font-weight: bold
-  color: #606266
-  margin-top: 0
-  margin-bottom: 4px
-.content
+  color: #909399
+.row
   display: flex
+  padding: 16px 0 8px
+  color: #606266
+  font-size: 14px
+  &+&
+    border-top: 1px solid #EBEEF5
+.name-status
+  flex: 1 0 180px
+  font-weight: bold
+.relationship
+  flex: 1 1 130px
+.duration
+  flex: 1 1 200px
+.company
+  flex: 1 1 240px
+.jobCategory
+  flex: 1 1 240px
+.belongs
+  flex: 1 1 200px
+.name
+  font-size: 16px
+  color: #606266
+.column
+  &+&
+    margin-left: 16px
 </style>
